0d133a77a83237b4bc2a82903b80f6ba6fb8803c
CBenetti <cinzia.benetti@edu.unito.it>
2024-09-20 20:39:27 +0200

    Last version

--- code/001_Trimming.sh
+++ code/001_Trimming.sh
@@ -14,6 +14,6 @@ module load trimmomatic/0.39
 		#module load python/cpu/3.10.6
 		#module load miniconda3/4.6.14
 		#conda activate trimmomatic_env
-		trimmomatic SE -phred33  data/Fastq/${ARG1} out/processed/Trimmed/${ARG1} ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+		java -jar /gpfs/share/apps/trimmomatic/0.39/trimmomatic-0.39.jar SE -phred33  data/Fastq/${ARG1} out/processed/Trimmed/${ARG1} ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
 		#conda deactivate
 		#module unload miniconda3/4.6.14
